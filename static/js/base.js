b85af13444730cc7695b65c589d65a1f529137c4
Pocary <andy52599731@gmail.com>
2023-07-05 23:18:34 +0900

    로그인 페이지 form 태그 배치, 회원가입 버튼 배치, 급식 페이지 디자인 통일.

--- static/js/base.js
+++ static/js/base.js
@@ -1,5 +1,9 @@
 window.addEventListener('load', function () {
     var curtain = document.querySelector('.curtain');
     
-    curtain.classList.add('fade-out');
+    curtain.classList.add('fade-in');
+    setTimeout(function () {
+        curtain.classList.add('fade-out');
+    }, 600);
+    
 });
