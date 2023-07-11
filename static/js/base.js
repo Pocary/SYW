3ba2419d436f601d2b762ecab2a8cf79c954fbb6
Pocary <andy52599731@gmail.com>
2023-07-12 01:46:23 +0900

    이게 왜 안돼

--- static/js/base.js
+++ static/js/base.js
@@ -1,9 +1,23 @@
 window.addEventListener('load', function () {
     var curtain = document.querySelector('.curtain');
     
-    curtain.classList.add('fade-in');
-    setTimeout(function () {
-        curtain.classList.add('fade-out');
-    }, 600);
+    // curtain.classList.add('fade-in');
+    // setTimeout(function () {
+    //     curtain.classList.add('fade-out');
+    // }, 600);
+    
     
 });
+
+function delayedRedirect(url, delay) {
+    setTimeout(function() {
+      window.location.href = url;
+    }, delay);
+  }
+$(function(){
+    $('.curtainButton').click(function(){
+      $('curtain').css('animation-play-state','running');
+    });
+  });
+
+  
